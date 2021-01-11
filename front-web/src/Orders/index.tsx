950925c659db123872b967d920f852595ab13273
chrmartinsgit config --global user.name chrmartinsgit config --global user.name chrmartins <cmts2010@gmail.com>
2021-01-11 11:37:00 -0300

    Fix total

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
-import StepsHeader from './StepsHeader'
+
+import { useEffect, useState } from 'react'
 import './styles.css'
+import StepsHeader from './StepsHeader'
+import ProductsList from './ProductsList'
+import { Product } from './types'
+import { fetchProducts } from '../api'
+import OrderLocation from './OrderLocation'
 
 
 function Orders() {
+
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
     return (
        <div className="orders-container">
          <StepsHeader />
+         <ProductsList products={products}/>
+         <OrderLocation />
        </div>
     )
 }
